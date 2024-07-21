d2d4722499836d63750e56249919f1943f2b4f2e
Odulapalli Hitesh <114851946+odulapalli1414@users.noreply.github.com>
2024-07-22 00:40:33 +0530

    Add files via upload

---
+++ Leetcode/Python/Q1470_Shuffle_the_Array.py
@@ -0,0 +1,12 @@
+def sol(nums, n):
+    res = []
+    for i in range(n):
+        res.append(nums[i])
+        res.append(nums[i+n])
+
+    return res
+
+nums = [2,5,1,3,4,7]
+n = 3
+
+print(sol(nums, n))
