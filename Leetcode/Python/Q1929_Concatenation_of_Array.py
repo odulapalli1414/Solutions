429c073d48ff5572e05c14aa7f6ff8b21446b647
Odulapalli Hitesh <114851946+odulapalli1414@users.noreply.github.com>
2024-07-22 00:45:23 +0530

    Create Q1929_Concatenation_of_Array.py

---
+++ Leetcode/Python/Q1929_Concatenation_of_Array.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # Concatenate the list with itself and return the result
+        return nums + nums
