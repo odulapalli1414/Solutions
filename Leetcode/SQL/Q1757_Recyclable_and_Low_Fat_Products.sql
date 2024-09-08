e5a5e9f66a3f05a4601d35359890efd5f217a42a
Odulapalli Hitesh <114851946+odulapalli1414@users.noreply.github.com>
2024-09-08 11:54:03 +0530

    Delete Leetcode/SQL directory

--- Leetcode/SQL/Q1757_Recyclable_and_Low_Fat_Products.sql
+++
@@ -1,7 +0,0 @@
--- Select the product_id from the Products table
-SELECT product_id 
-FROM Products
--- Where the product has low fats (denoted by 'Y')
-WHERE low_fats = 'Y' 
--- And the product is recyclable (denoted by 'Y')
-AND recyclable = 'Y';
