a4cbd05d475a023c18fd3314fea02529efb6e0f6
Odulapalli Hitesh <114851946+odulapalli1414@users.noreply.github.com>
2025-02-19 21:03:45 +0530

    Create Q121_Best_Time_to_Buy_and_Sell_Stock.java

---
+++ Leetcode/Java/Q121_Best_Time_to_Buy_and_Sell_Stock.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices==null || prices.length == 0){
+            return 0;
+        }
+
+        int max_profit = 0;
+        int min_price = Integer.MAX_VALUE;
+
+        for(int price : prices){
+            if(price < min_price){
+                min_price = price;
+            }
+            else{
+                max_profit = Math.max(max_profit, price-min_price);
+            }
+        }
+        return max_profit;
+    }
+}
